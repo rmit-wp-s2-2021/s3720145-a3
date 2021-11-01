e2def62ed56afcc58b6ec8e76f92c50bd24646d0
s3720145 <41350333+s3720145@users.noreply.github.com>
2021-11-02 04:19:44 +1100

    can now retrieve weather related information.
    TODO: The retrieved information needs to be rendered in a friendlier format.

--- a3/weather.php
+++ a3/weather.php
@@ -1,6 +1,9 @@
+<!-- create an array to store strings of the locations of interest -->
 <?php
     $cities = array("Sydney, Australia" , "Tokyo, Japan", "Barcelona, Spain");
 ?>
+
+<!-- api password: wp-pass12 -->
 
 <!DOCTYPE html>
 <html>
@@ -12,13 +15,22 @@
 
 <body>
     <div>
-        <h1> Weather of cites </h1>
+        <h1> Current Weather of cites </h1>
     <div>
 
     <div class="city_container">
         <?php foreach($cities as $city) { ?>
             <div class="city_box">
                 <h3><?php echo $city ?></h3>
+                <?php 
+                    $url = "api.openweathermap.org/data/2.5/weather?q={$city}&appid=85c116df3468356cb87a423b00347fc4";
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_URL, $url);
+                    $response = curl_exec($ch);
+                    $arr_result = json_decode($response);
+                    print_r($arr_result);
+                ?>
             </div>
         <?php } ?>
     </div>
